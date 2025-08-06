044bd8f4caf1c357c4f97de02f16d8d678a82348
alankritdabral <95607957+alankritdabral@users.noreply.github.com>
2025-08-06 15:04:53 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `Hello! I’m a final-semester student with a strong passion for web development and AI/ML. I’ve gained hands-on experience in technologies such as React.js, Django, MySql. My goal is to become a skilled full-stack app developer, capable of building dynamic, user-friendly web applications. As I approach the completion of my degree, I’m excited to apply my knowledge in both front-end and back-end development to create efficient solutions and contribute to projects that push technological innovation and enhance user experiences.`;
 
